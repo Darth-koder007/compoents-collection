2191828a63b0db637876c6b5dc91e5143bbc83a1
vijay.singh <vijay.singh@inmobi.com>
2020-06-03 17:25:40 +0530

    gh-pages

--- src/App.js
+++ src/App.js
@@ -10,63 +10,6 @@ const App = () => {
       <PDFViewer file={sample} />
       <hr />
       <PDFViewer file={sampleKA} />
-      {/* <FScreen>
-        <div className="doc-container">
-          <Document
-            file={sample}
-            onLoadSuccess={onDocumentLoadSuccess}
-            options={options}
-          >
-            <Page pageNumber={pageNumber} className="custom-page" />
-            <p className="nav-controls">
-              <button
-                type="button"
-                onClick={() => setPageNumber(pageNumber - 1)}
-              >
-                Prev
-              </button>
-              <span>
-                Page {pageNumber} of {numPages}
-              </span>
-              <button
-                type="button"
-                onClick={() => setPageNumber(pageNumber + 1)}
-              >
-                Next
-              </button>
-            </p>
-          </Document>
-        </div>
-      </FScreen>
-      <hr />
-      <FScreen>
-        <div className="doc-container">
-          <Document
-            file={sampleKA}
-            onLoadSuccess={onDocumentLoadSuccess}
-            options={options}
-          >
-            <Page pageNumber={pageNumber} />
-            <p className="nav-controls">
-              <button
-                type="button"
-                onClick={() => setPageNumber(pageNumber - 1)}
-              >
-                Prev
-              </button>
-              <span>
-                Page {pageNumber} of {numPages}
-              </span>
-              <button
-                type="button"
-                onClick={() => setPageNumber(pageNumber + 1)}
-              >
-                Next
-              </button>
-            </p>
-          </Document>
-        </div>
-      </FScreen> */}
     </div>
   );
 };
